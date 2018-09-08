f3bd2427305496af7e43f6782bce2da805cdaf51
Joseantonio Maciel <joseantoniomaciel@Joseantonios-MacBook-Air.local>
2018-09-07 18:41:34 -0700

    update string format

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var TipLabel: UILabel!
+    @IBOutlet weak var TotalLabel: UILabel!
+    @IBOutlet weak var BillField: UITextField!
+    @IBOutlet weak var TipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentage = [0.18, 0.20, 0.25]
+        
+        let bill = Double( BillField.text!) ?? 0
+        
+        let tip = bill * tipPercentage[TipControl.selectedSegmentIndex]
+        let total = bill + tip
+        TipLabel.text = String (format:"$%.2f", tip)
+        TotalLabel.text = String (format:"$%.2f", total)
+    }
 }
 
